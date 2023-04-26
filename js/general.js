9eb9280288d99377b2fc95842231b1ad8991d29a
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-04-26 21:48:06 +0200

    added searchbar

--- js/general.js
+++ js/general.js
@@ -1,7 +1,14 @@
 const menuButton = document.querySelector(".menu-button");
 const menuChoices = document.querySelector(".menu-choices");
 
+const searchIcon = document.querySelector(".search-icon");
+const searchBar = document.querySelector(".search-bar");
+
 menuButton.onclick = function () {
   menuChoices.classList.toggle("show");
+};
+
+searchIcon.onclick = function () {
   console.log("hello");
+  searchBar.classList.toggle("show");
 };
