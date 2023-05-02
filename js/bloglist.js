091e4e92eeb7fb342da4add39da8b481dc6779e1
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-05-02 21:48:57 +0200

    added bloglist and cards

---
+++ js/bloglist.js
@@ -0,0 +1,20 @@
+const url =
+  "https://traveller-api.lindaas.net/wp-json/wp/v2/posts?_embed&per_page=10";
+const blogpostHtml = document.querySelector(".posts");
+
+async function getBlogPosts() {
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+  showBlogPosts(result);
+}
+getBlogPosts();
+
+function showBlogPosts(blogposts) {
+  for (let i = 0; i < blogposts.length; i++) {
+    console.log(blogposts[i].title.rendered);
+    const title = `<li><div class="card"><img src="${blogposts[i]._embedded["wp:featuredmedia"]["0"].source_url}" class="featured-image"/><div>${blogposts[i].title.rendered}</div></div></li>`;
+    blogpostHtml.innerHTML += title;
+  }
+}
+//._embedded['wp:featuredmedia']['0'].source_url
