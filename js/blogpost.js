0d1ea2e386756c879c726ad508ca83e2693d4f4e
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-05-08 21:09:26 +0200

    added blogpost

--- js/blogpost.js
+++ js/blogpost.js
@@ -2,12 +2,32 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
-const url = "https://traveller-api.lindaas.net/wp-json/wp/v2/posts?_embed" + id;
+const url =
+  "https://traveller-api.lindaas.net/wp-json/wp/v2/posts/" + id + "?_embed";
 
 const post = document.querySelector(".post");
+const featuredImage = document.querySelector(".image-header");
+const headline = document.querySelector(".headline");
 
 async function getPost() {
   const response = await fetch(url);
   const result = await response.json();
-  console.log("result");
+  showHeadline(result);
+  showFeaturedImage(result);
+  showPost(result);
 }
+
+function showFeaturedImage(image) {
+  console.log("jgyf", image._embedded["wp:featuredmedia"]["0"].source_url);
+  featuredImage.style.backgroundImage = `url(${image._embedded["wp:featuredmedia"]["0"].source_url})`;
+}
+function showHeadline(blogpost) {
+  //console.log(blogpost.title);
+  headline.innerHTML = blogpost.title.rendered;
+}
+function showPost(blogpost) {
+  //   console.log(blogpost);
+  post.innerHTML = blogpost.content.rendered;
+}
+
+getPost();
